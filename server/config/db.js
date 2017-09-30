701e97d26ba6b92326ade2fed255c61b911da331
Charley Pinckney <pinckc7@gmail.com>
2017-09-30 14:00:32 -0400

    attempting to use only JAWSDB_URL

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,15 @@ var mysql = require('mysql');
 
 //=============FOR RDS DATABASE===================//
 
-var pool = mysql.createPool({
-    connectionLimit:10,
-    host: process.env.HEROKU_DB_HOSTNAME,
-    user: process.env.HEROKU_DB_USERNAME,
-    password: process.env.HEROKU_DB_PASSWORD,
-    database: process.env.HEROKU_DB_DATABASE
-});
+var pool = mysql.createPool(
+    process.env.JAWSDB_URL + '?connectionLimit=10' 
+
+    // connectionLimit:10,
+    // host: process.env.HEROKU_DB_HOSTNAME,
+    // user: process.env.HEROKU_DB_USERNAME,
+    // password: process.env.HEROKU_DB_PASSWORD,
+    // database: process.env.HEROKU_DB_DATABASE
+);
 
 //-------------------------------------------------------
 
