5c25f4220161b17d4085fb70a34774bc9864f269
Charley Pinckney <pinckc7@gmail.com>
2018-01-31 04:19:37 -0600

    deactivated emailService until after testing phase

--- server/services/email.svc.js
+++ server/services/email.svc.js
@@ -8,45 +8,45 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
                     //REINSTATE THIS FUNCTION AFTER TESTING PERIOD IS OVER!!!
 //==============================================================================================
 
-exports.sendEmail = function(toAddress, fromAddress, subject, content){
+// exports.sendEmail = function(toAddress, fromAddress, subject, content){
 
 
-        const msg = {
-        to: toAddress,
-        from: 'hlawrence@exceptionalfoundation.org',
-        subject: subject,
-        text: content,
-        };
-        sgMail.send(msg).then(function(success){
-            console.log('email sent successfully');
-        }).catch(function(error){
-            console.log(error);
-            console.log(error.response.body);
+//         const msg = {
+//         to: toAddress,
+//         from: 'hlawrence@exceptionalfoundation.org',
+//         subject: subject,
+//         text: content,
+//         };
+//         sgMail.send(msg).then(function(success){
+//             console.log('email sent successfully');
+//         }).catch(function(error){
+//             console.log(error);
+//             console.log(error.response.body);
 
-        })
-
-
-
-}
-
-exports.sendNewPwordEmail = function(userEmail, newPassword, userName){
-
-        const msg = {
-            to: userEmail,
-            from: 'hlawrence@exceptionalfoundation.org',
-            subject: 'new password',
-            text: 'Your account info has been updated. Your login credentials are: '+ 'login: ' +userName + ' password: '  + newPassword,
-        };
-        sgMail.send(msg).then(function(success){
-            console.log('email sent successfully');
-        }).catch(function(error){
-            console.log(error);
-            console.log(error.response.body);
-
-        })
+//         })
 
 
-}
+
+// }
+
+// exports.sendNewPwordEmail = function(userEmail, newPassword, userName){
+
+//         const msg = {
+//             to: userEmail,
+//             from: 'hlawrence@exceptionalfoundation.org',
+//             subject: 'new password',
+//             text: 'Your account info has been updated. Your login credentials are: '+ 'login: ' +userName + ' password: '  + newPassword,
+//         };
+//         sgMail.send(msg).then(function(success){
+//             console.log('email sent successfully');
+//         }).catch(function(error){
+//             console.log(error);
+//             console.log(error.response.body);
+
+//         })
+
+
+// }
 
 
 //---------------------------------------------------------------------------------------------------
