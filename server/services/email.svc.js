29dd854172e04bb9dd9caa53882345f941ea2290
Charley Pinckney <pinckc7@gmail.com>
2018-01-30 21:00:15 -0600

    deactivated the emailService when adding users

--- server/services/email.svc.js
+++ server/services/email.svc.js
@@ -3,29 +3,31 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-exports.sendEmail = function(toAddress, fromAddress, subject, content){
 
-
-        const msg = {
-        to: toAddress,
-        from: fromAddress,
-        subject: subject,
-        text: content,
-        };
-        sgMail.send(msg).then(function(success){
-            console.log('email sent successfully');
-        }).catch(function(error){
-            console.log(error);
-            console.log(error.response.body);
-
-        })
-
-
-
-}
 
                     //REINSTATE THIS FUNCTION AFTER TESTING PERIOD IS OVER!!!
 //==============================================================================================
+
+// exports.sendEmail = function(toAddress, fromAddress, subject, content){
+
+
+//         const msg = {
+//         to: toAddress,
+//         from: fromAddress,
+//         subject: subject,
+//         text: content,
+//         };
+//         sgMail.send(msg).then(function(success){
+//             console.log('email sent successfully');
+//         }).catch(function(error){
+//             console.log(error);
+//             console.log(error.response.body);
+
+//         })
+
+
+
+// }
 
 // exports.sendNewPwordEmail = function(userEmail, newPassword){
 
